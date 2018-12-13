cf35b0ed5e50fc403bc78b01e44084ce017091e2
Mohanavadivel-git <mohan.vadivel@gmail.com>
2018-12-13 21:15:32 +0530

    Fixed PUT and GetbyID issue

--- src/main/java/com/example/Personal/Repositories/BlogRespository.java
+++ src/main/java/com/example/Personal/Repositories/BlogRespository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @Repository
 public interface BlogRespository extends JpaRepository<Blog, Integer> {
-
     // custom query to search to blog post by title or content
     List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
     Blog findById(int Id);
