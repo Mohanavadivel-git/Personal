63d4726e828737b1b80796bdd99561901682a2c3
Mohanavadivel-git <mohan.vadivel@gmail.com>
2019-01-20 18:27:34 +0530

    Added few more update APIs

--- src/main/java/com/example/Personal/Controller/LeprosyController.java
+++ src/main/java/com/example/Personal/Controller/LeprosyController.java
@@ -39,8 +39,10 @@ public class LeprosyController {
     @PutMapping("/Leprosy/{id}")
     public Leprosy update(@PathVariable String id, @RequestBody Leprosy body){
         int LeprosyId = Integer.parseInt(id);
+        //body.setId(id);
         // getting blog
         Leprosy LeprosyUpdate = LeprosyRespository.findById(LeprosyId);
+        //LeprosyUpdate=body;
         LeprosyUpdate.id=LeprosyId;
         LeprosyUpdate.state=body.state;
         LeprosyUpdate.district=body.district;
@@ -60,6 +62,67 @@ public class LeprosyController {
         LeprosyUpdate.dose15mg=body.dose15mg;
         LeprosyUpdate.dose10mg=body.dose10mg;
         LeprosyUpdate.dose5mg=body.dose5mg;
+        LeprosyUpdate.otherdrugs=body.otherdrugs;
+        LeprosyUpdate.treatmentcomplete=body.treatmentcomplete;
+        LeprosyUpdate.personstatus=body.personstatus;
+        LeprosyUpdate.remarks=body.remarks;
+        return LeprosyRespository.save(LeprosyUpdate);
+    }
+
+    @PutMapping("/Leprosy/contacts/{id}")
+    public Leprosy updatecontacts(@PathVariable String id, @RequestBody Leprosy body){
+        int LeprosyId = Integer.parseInt(id);
+        //body.setId(id);
+        // getting blog
+        Leprosy LeprosyUpdate = LeprosyRespository.findById(LeprosyId);
+        //LeprosyUpdate=body;
+        LeprosyUpdate.id=LeprosyId;
+        LeprosyUpdate.state=body.state;
+        LeprosyUpdate.district=body.district;
+        LeprosyUpdate.healthfacility=body.healthfacility;
+        LeprosyUpdate.phonenumber=body.phonenumber;
+        LeprosyUpdate.address=body.address;
+        return LeprosyRespository.save(LeprosyUpdate);
+    }
+
+    @PutMapping("/Leprosy/leprosy/{id}")
+    public Leprosy updateleprosy(@PathVariable String id, @RequestBody Leprosy body){
+        int LeprosyId = Integer.parseInt(id);
+        //body.setId(id);
+        // getting blog
+        Leprosy LeprosyUpdate = LeprosyRespository.findById(LeprosyId);
+        //LeprosyUpdate=body;
+        LeprosyUpdate.id=LeprosyId;
+        LeprosyUpdate.leprosytype=body.leprosytype;
+        LeprosyUpdate.identifiedtime= body.identifiedtime;
+        LeprosyUpdate.disabilitystatus=body.disabilitystatus;
+        LeprosyUpdate.reactiontype=body.reactiontype;
+        return LeprosyRespository.save(LeprosyUpdate);
+    }
+
+    @PutMapping("/Leprosy/dosage/{id}")
+    public Leprosy updatedosage(@PathVariable String id, @RequestBody Leprosy body){
+        int LeprosyId = Integer.parseInt(id);
+        //body.setId(id);
+        // getting blog
+        Leprosy LeprosyUpdate = LeprosyRespository.findById(LeprosyId);
+        //LeprosyUpdate=body;
+        LeprosyUpdate.dose40mg=body.dose40mg;
+        LeprosyUpdate.dose30mg=body.dose30mg;
+        LeprosyUpdate.dose20mg=body.dose20mg;
+        LeprosyUpdate.dose15mg=body.dose15mg;
+        LeprosyUpdate.dose10mg=body.dose10mg;
+        LeprosyUpdate.dose5mg=body.dose5mg;
+        return LeprosyRespository.save(LeprosyUpdate);
+    }
+
+    @PutMapping("/Leprosy/others/{id}")
+    public Leprosy updateothers(@PathVariable String id, @RequestBody Leprosy body){
+        int LeprosyId = Integer.parseInt(id);
+        //body.setId(id);
+        // getting blog
+        Leprosy LeprosyUpdate = LeprosyRespository.findById(LeprosyId);
+        //LeprosyUpdate=body;
         LeprosyUpdate.otherdrugs=body.otherdrugs;
         LeprosyUpdate.treatmentcomplete=body.treatmentcomplete;
         LeprosyUpdate.personstatus=body.personstatus;
